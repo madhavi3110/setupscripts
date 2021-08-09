ac18084497ca18f49daf26c1d03b7d1b9a8668c8
madhavi3110 <47594154+madhavi3110@users.noreply.github.com>
2021-08-09 21:51:00 +0530

    Update dockerscript.sh

--- dockerscript.sh
+++ dockerscript.sh
@@ -14,8 +14,8 @@ echo \
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io -y
 # Docker compose commands
-sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
+#sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+#sudo chmod +x /usr/local/bin/docker-compose
 
 sudo mkdir /etc/docker
 cat <<EOF | sudo tee /etc/docker/daemon.json
