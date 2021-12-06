6a92014345d4fc856fe8763442c8b841e7bd4e5a
madhavi3110 <47594154+madhavi3110@users.noreply.github.com>
2021-12-06 15:30:47 +0530

    Update kubernetesscript.sh

--- kubernetesscript.sh
+++ kubernetesscript.sh
@@ -1,3 +1,12 @@
+## The below are the commands for Installing Docker and Kubernetes (K8S) on EC2 instance. 
+## Both Master and Worker nodes the follwoing commands has to be executed.
+sudo apt-get update
+sudo apt-get install     apt-transport-https     ca-certificates     curl     gnupg     lsb-release
+echo   "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+## Kubernetes Installing
 sudo apt-get update
 sudo apt-get install -y apt-transport-https ca-certificates curl
 sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
@@ -5,3 +14,15 @@ echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https:/
 sudo apt-get update
 sudo apt-get install -y kubelet kubeadm kubectl
 sudo apt-mark hold kubelet kubeadm kubectl
+   
+##### The follwoing commands has to execute in Master node.
+   
+kubeadm init (Only Init has to Run as Root user and get the information from the output and the join command need to run on Worker node) 
+# the belwo commands run as normal user
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+####### the below commad is for network please run appropriate network command (
+kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')" 
+kubectl get nodes  
+   
